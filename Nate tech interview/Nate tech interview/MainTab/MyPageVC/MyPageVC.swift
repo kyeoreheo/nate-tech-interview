f63f16cbf90b1f030c285bfa41f8846496904082
kyeoreheo <91kyoheo@gmail.com>
2020-11-08 13:55:34 -0500

    basic layout for mypage

--- Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVC.swift
+++ Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVC.swift
@@ -8,9 +8,78 @@
 import UIKit
 
 class MyPageVC: UIViewController {
-    private let viewModel = MyPageVM()
+    // MARK:- View components
+    private let titleLabel = UILabel()
+    private lazy var usernameInfoField = viewModel.informationField(
+                     labelText: "username", textFieldText: "abc")
+    private lazy var emailInfoField = viewModel.informationField(
+                     labelText: "email", textFieldText: "abc")
+    private lazy var addressInfoField = viewModel.informationField(
+                     labelText: "address", textFieldText: "45 River, Jersey City, NJ, 07310",
+                     action: #selector(changeAddress), target: self)
+    private lazy var cardInfoField = viewModel.informationField(
+                     labelText: "card", textFieldText: "**** - **** - **** - 1234",
+                     action: #selector(changeAddress), target: self)
     
+    // MARK:- Properties
+    private lazy var viewModel = MyPageVM(self)
+    
+    // MARK:- Lifecycle
     override func viewDidLoad() {
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
+        configureUI()
     }
+    
+    // MARK:- Configures
+    private func configureUI() {
+        view.addSubview(titleLabel)
+        titleLabel.text = "My Page"
+        titleLabel.textColor = .gray8
+        titleLabel.font = .notoBlack(size: 24 * ratio)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(4)
+            make.left.equalToSuperview().offset(24)
+        }
+        
+        view.addSubview(usernameInfoField)
+        usernameInfoField.snp.makeConstraints { make in
+            make.height.equalTo(88 * ratio)
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        view.addSubview(emailInfoField)
+        emailInfoField.snp.makeConstraints { make in
+            make.height.equalTo(88 * ratio)
+            make.top.equalTo(usernameInfoField.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        view.addSubview(addressInfoField)
+        addressInfoField.snp.makeConstraints { make in
+            make.height.equalTo(88 * ratio)
+            make.top.equalTo(emailInfoField.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        view.addSubview(cardInfoField)
+        cardInfoField.snp.makeConstraints { make in
+            make.height.equalTo(88 * ratio)
+            make.top.equalTo(addressInfoField.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+    }
+    
+    @objc func changeAddress() {
+        
+    }
+    
+    @objc func changeCreditCard() {
+        
+    }
+
 }
--- Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVM.swift
+++ Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVM.swift
@@ -8,5 +8,75 @@
 import UIKit
 
 class MyPageVM {
+    private let viewController: Any
     
+    init(_ vc: Any) {
+        self.viewController = vc
+    }
+    /// InformationTextField with button
+    func informationField(labelText: String, textFieldText: String, action: Selector? = nil, target: Any? = nil) -> UIView {
+        let view = UIView()
+        let label = UILabel()
+        let button = UIButton()
+        let frame = UIImageView()
+        let textFieldLabel = UILabel()
+        
+        view.addSubview(label)
+        label.text = labelText
+        label.font = UIFont.notoReg(size: 14 * ratio)
+        label.textColor = .gray8
+        label.adjustsFontSizeToFitWidth = true
+        label.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+        
+        if let action = action, let target = target {
+            view.addSubview(button)
+            button.setTitle("EDIT", for: .normal)
+            button.setTitleColor(.gray4, for: .normal)
+            button.titleLabel?.font = UIFont.notoBold(size: 14 * ratio)
+            button.layer.borderColor = UIColor.gray4.cgColor
+            button.layer.borderWidth = 1.5
+            button.layer.cornerRadius = 8
+            button.addTarget(target, action: action, for: .touchUpInside)
+            button.snp.makeConstraints { make in
+                make.width.equalTo(90 * ratio)
+                make.height.equalTo(56 * ratio)
+                make.top.equalTo(label.snp.bottom).offset(12)
+                make.bottom.equalToSuperview()
+                make.right.equalToSuperview()
+            }
+        }
+        
+        view.addSubview(frame)
+        frame.backgroundColor = .gray2
+        frame.layer.borderColor = UIColor.gray4.cgColor
+        frame.layer.borderWidth = 1.5
+        frame.layer.cornerRadius = 8
+        frame.snp.makeConstraints { make in
+            make.height.equalTo(56 * ratio)
+            make.top.equalTo(label.snp.bottom).offset(12)
+            make.left.equalToSuperview()
+            if action == nil {
+                make.right.equalToSuperview()
+            } else {
+                make.right.equalTo(button.snp.left).offset(-8)
+            }
+        }
+        
+        view.addSubview(textFieldLabel)
+        textFieldLabel.tag = 1
+        textFieldLabel.text = textFieldText
+        textFieldLabel.font = UIFont.notoReg(size: 16)
+        textFieldLabel.textColor = .gray5
+        textFieldLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(frame.snp.centerY)
+            make.left.equalTo(frame.snp.left).offset(16)
+            make.right.equalTo(frame.snp.right).offset(-8)
+        }
+
+        return view
+    }
 }
