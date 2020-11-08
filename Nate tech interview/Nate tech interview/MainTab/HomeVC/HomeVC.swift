a38235daddb026cf72cb055bba7a3d6c1710751c
kyeoreheo <91kyoheo@gmail.com>
2020-11-08 18:40:56 -0500

    refactor code

--- Nate tech interview/Nate tech interview/MainTab/HomeVC/HomeVC.swift
+++ Nate tech interview/Nate tech interview/MainTab/HomeVC/HomeVC.swift
@@ -9,9 +9,12 @@ import UIKit
 import SnapKit
 
 class HomeVC: UIViewController {
+    // MARK:- View components
+    private let titleLabel = UILabel()
+    private let productFeedCVC = ProductFeedCVC()
+
     // MARK:- Properties
     private lazy var viewModel = HomeVM(self)
-    private let productFeedCVC = ProductFeedCVC()
     var products = [API.ProductResponse]() {
         didSet {
             productFeedCVC.products = products
@@ -26,10 +29,19 @@ class HomeVC: UIViewController {
     
     // MARK:- Configures
     private func configureUI() {
+        view.addSubview(titleLabel)
+        titleLabel.text = "Trand Items"
+        titleLabel.textColor = .gray8
+        titleLabel.font = .notoBlack(size: 24 * ratio)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(4)
+            make.left.equalToSuperview().offset(24)
+        }
+        
         view.backgroundColor = .white
         view.addSubview(productFeedCVC.view)
         productFeedCVC.view.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
             make.left.right.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
--- Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVC.swift
+++ Nate tech interview/Nate tech interview/MainTab/MyPageVC/MyPageVC.swift
@@ -10,16 +10,22 @@ import UIKit
 class MyPageVC: UIViewController {
     // MARK:- View components
     private let titleLabel = UILabel()
+    private let versionLabel = UILabel()
     private lazy var usernameInfoField = viewModel.informationField(
-                     labelText: "username", textFieldText: "abc")
+                     labelText: "username", textFieldText: "Kyeore Heo")
     private lazy var emailInfoField = viewModel.informationField(
-                     labelText: "email", textFieldText: "abc")
+                     labelText: "email", textFieldText: "91kyoheo@gmail.com")
     private lazy var addressInfoField = viewModel.informationField(
                      labelText: "address", textFieldText: "45 River, Jersey City, NJ, 07310",
                      action: #selector(changeAddress), target: self)
     private lazy var cardInfoField = viewModel.informationField(
                      labelText: "card", textFieldText: "**** - **** - **** - 1234",
                      action: #selector(changeAddress), target: self)
+    private lazy var phoneInfoField = viewModel.informationField(
+                     labelText: "phone", textFieldText: "+1 917-123-1234",
+                     action: #selector(changePhone), target: self)
+    private let msgNotificationSwitch = UISwitch()
+    private let msgNotificationLabel = UILabel()
     
     // MARK:- Properties
     private lazy var viewModel = MyPageVM(self)
@@ -39,6 +45,17 @@ class MyPageVC: UIViewController {
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(4)
             make.left.equalToSuperview().offset(24)
+        }
+        
+        guard let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        else { return }
+        view.addSubview(versionLabel)
+        versionLabel.text = "\(String(describing: appVersion))(iOS\(UIDevice.current.systemVersion))"
+        versionLabel.font = UIFont.notoReg(size: 12 * ratio)
+        versionLabel.textColor = .gray5
+        versionLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(4)
+            make.right.equalToSuperview().offset(-24)
         }
         
         view.addSubview(usernameInfoField)
@@ -72,6 +89,43 @@ class MyPageVC: UIViewController {
             make.left.equalToSuperview().offset(24)
             make.right.equalToSuperview().offset(-24)
         }
+        
+        view.addSubview(phoneInfoField)
+        phoneInfoField.snp.makeConstraints { make in
+            make.height.equalTo(88 * ratio)
+            make.top.equalTo(cardInfoField.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        view.addSubview(msgNotificationSwitch)
+        msgNotificationSwitch.tintColor = .gray3
+        msgNotificationSwitch.onTintColor = .green
+        msgNotificationSwitch.addTarget(self, action: #selector(toggleNotification), for: .touchUpInside)
+        msgNotificationSwitch.snp.makeConstraints { make in
+            make.height.equalTo(31 * ratio)
+            make.top.equalTo(phoneInfoField.snp.bottom).offset(20.5)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        view.addSubview(msgNotificationLabel)
+        msgNotificationLabel.text = "Do you want to get notified with message\nabout delivery status?"
+        msgNotificationLabel.numberOfLines = 0
+        msgNotificationLabel.font = UIFont.notoReg(size: 14 * ratio)
+        msgNotificationLabel.textColor = .gray8
+        msgNotificationLabel.snp.makeConstraints { make in
+            make.top.equalTo(phoneInfoField.snp.bottom).offset(8)
+            make.left.equalToSuperview().offset(24)
+            make.right.equalTo(msgNotificationSwitch.snp.left).offset(-8)
+        }
+    }
+    
+    @objc func toggleNotification() {
+        
+    }
+    
+    @objc func changePhone() {
+        
     }
     
     @objc func changeAddress() {
